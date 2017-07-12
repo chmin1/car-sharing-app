1d55fa1ff0a5988c2fc32121ef28bdd1fa70e5d8
Chavane Minto <chevy@fb.com>
2017-07-12 11:22:51 -0700

    NSObject that adds user's new trip info to Parse server

--- CarSharingApp/Trip.swift
+++ CarSharingApp/Trip.swift
@@ -7,7 +7,39 @@
 //
 
 import UIKit
+import Parse
+
 
 class Trip: NSObject {
+    
+    // Method to upload user trip to Parse
+    
+    /*
+     
+     Parameters:
+        - User can add Departure Location (From)
+        - User can add Arrival location (To)
+        - User can add the Earliest & Latest times of departure
+     
+    */
+    
+    class func postTrip(withDeparture departureLoc: String?, withArrival arrivalLoc: String?, withEarliest earlyDepart: NSDate?, withLatest lateDepart: NSDate?, withCompletion completion: PFBooleanResultBlock?) {
+        
+        // Create Trip Object: PFObject
+        let trip = PFObject(className: "Trip")
+        
+        //Add relevant fields to the object
+        trip["Planner"] = PFUser.current()
+        trip["DepartureLoc"] = departureLoc // Location where you will leave from
+        trip["ArrivalLoc"] = arrivalLoc // Location you will arrive to
+        trip["EarliestTime"] = earlyDepart // Earliest time you can leave
+        trip["LatestTime"] = lateDepart // Latest timne you can leave
+        
+        // Save object (following function will save the object in Parse asynchronously)
+        trip.saveInBackground { (success: Bool, error: Error?) in
+            completion?(success, error)
+        }
+        
+    }
 
 }
