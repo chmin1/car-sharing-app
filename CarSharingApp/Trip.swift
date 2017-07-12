1abccb8d4a33cd54dcb29cf05a59b5baaddef327
Chavane Minto <chevy@fb.com>
2017-07-12 11:20:26 -0700

    Added file: Trip.swift

---
+++ CarSharingApp/Trip.swift
@@ -0,0 +1,13 @@
+//
+//  Trip.swift
+//  CarSharingApp
+//
+//  Created by Chavane Minto on 7/12/17.
+//  Copyright © 2017 FBU. All rights reserved.
+//
+
+import UIKit
+
+class Trip: NSObject {
+
+}
