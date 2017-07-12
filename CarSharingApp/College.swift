d187d5a34db1f0453f6958db7203b879b704bd93
Chavane Minto <chevy@fb.com>
2017-07-12 15:29:32 -0700

    Created class that holds a array of Colleges (JSON objects)

--- CarSharingApp/College.swift
+++ CarSharingApp/College.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+class College {
+    
+    var alphaCode: String
+    var country: String
+    var domain: String
+    var name: String
+    
+    class func colleges(dictionaries: [[String: Any]]) -> [College] {
+        var colleges: [College] = []
+        for dictionary in dictionaries {
+            let college = College(dictionary: dictionary)
+            colleges.append(college)
+        }
+        
+        return colleges
+    }
+    
+    init(dictionary: [String: Any]) {
+        
+        alphaCode = dictionary["alpha_two_code"] as! String
+        country = dictionary["country"] as! String
+        domain = dictionary["domain"] as! String
+        name = dictionary["name"] as! String
+        
+    }
+    
+}
