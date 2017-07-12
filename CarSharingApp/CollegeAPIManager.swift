7035dadd6be6f4048c3a2dcff34b53eff55f345b
Chavane Minto <chevy@fb.com>
2017-07-12 15:33:07 -0700

    API manager that handles URL request for college information to populate College class.

--- CarSharingApp/CollegeAPIManager.swift
+++ CarSharingApp/CollegeAPIManager.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+class CollegeAPIManager {
+    
+    // URL that holds info on all colleges
+    static let collegeUrl = "https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json"
+    
+    var session: URLSession
+    
+    init() {
+        session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+    }
+    
+    func verifiedColleges(completion: @escaping ([College]?, Error?) -> ()) {
+        
+        let request = URLRequest(url: URL(string: CollegeAPIManager.collegeUrl)!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            if let data = data {
+                
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let collegeDictionaries = dataDictionary[""] as! [[String: Any]]
+                
+                let colleges = College.colleges(dictionaries: collegeDictionaries)
+                completion(colleges, nil)
+                
+            } else {
+                
+                completion(nil, error)
+                
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+}
