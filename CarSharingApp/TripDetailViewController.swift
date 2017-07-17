363fc9ce255c00dfc69c445743b9955236dcf9ca
Elan Halpern <elanh@fb.com>
2017-07-17 12:15:57 -0700

    Created the trip details view controller

---
+++ CarSharingApp/TripDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TripDetailViewController.swift
+//  CarSharingApp
+//
+//  Created by Elan Halpern on 7/17/17.
+//  Copyright © 2017 FBU. All rights reserved.
+//
+
+import UIKit
+
+class TripDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
