57e2d3460d7a137fd7e3a460a62dac0b8e008906
Chavane Minto <chevy@fb.com>
2017-07-17 12:46:28 -0700

    Added UICollectionViewCell: messagesCell.swift

---
+++ CarSharingApp/messagesCell.swift
@@ -0,0 +1,13 @@
+//
+//  messagesCell.swift
+//  CarSharingApp
+//
+//  Created by Chavane Minto on 7/17/17.
+//  Copyright © 2017 FBU. All rights reserved.
+//
+
+import UIKit
+
+class messagesCell: UICollectionViewCell {
+    
+}
