73fe9e3f9a79c61f09a4e3821aa10c43a21a1089
Chavane Minto <chevy@fb.com>
2017-07-18 10:21:31 -0700

    This is a dummy class with the purpose of testing how to ceate a conversation object in parse. It will be deleted later

--- CarSharingApp/CreateConvoViewController.swift
+++ CarSharingApp/CreateConvoViewController.swift
@@ -9,11 +9,35 @@
 import UIKit
 
 class CreateConvoViewController: UIViewController {
+    
+    // THIS IS A DUMMY CLASS THAT WILL BE DELETED LATER ALONG WITH ITS VIEW CONTROLLER *****
+    // THIS CLASS IS FOR TESTING PURPOSES ONLY *********************************************
+    
+    @IBOutlet weak var Rider1Field: UITextField!
+    
+    @IBOutlet weak var Rider2Field: UITextField!
+    
+    @IBOutlet weak var Rider3Field: UITextField!
+    
+    @IBOutlet weak var Rider4Field: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func onScreenTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func onSubmitConvo(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onDismiss(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
