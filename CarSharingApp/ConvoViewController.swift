d7640ebd039c03e342f64abff61a60b402955ed5
Chavane Minto <chevy@fb.com>
2017-07-20 13:38:47 -0700

    Added text message dock for typing messages and sending them

--- CarSharingApp/ConvoViewController.swift
+++ CarSharingApp/ConvoViewController.swift
@@ -9,19 +9,36 @@
 import UIKit
 import Parse
 
-class ConvoViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class ConvoViewController: UIViewController, UITextViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var convoView: UICollectionView!
     
+    @IBOutlet weak var Dock: UIView!
+    
     @IBOutlet weak var messageField: UITextView!
     
+    @IBOutlet weak var sendButton: UIButton!
+    
     var Trip: PFObject!
+    
+    var previousRect: CGRect!
+    var returnPressed: Int = 0
+    var newLine: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         convoView.delegate = self
         convoView.dataSource = self
+        messageField.delegate = self
+        
+        previousRect = CGRect.zero
+        messageField.layer.masksToBounds = true
+        messageField.layer.borderWidth = 1.0
+        messageField.layer.borderColor = UIColor.black.cgColor
+        messageField.layer.cornerRadius = 6
+        messageField.textColor = UIColor.lightGray
+        messageField.text = "Compose a message..."
 
         if let title = Trip["Name"] as? String {
             print(title)
@@ -35,6 +52,80 @@ class ConvoViewController: UIViewController, UICollectionViewDelegate, UICollect
         let maxHeight:CGFloat = messageField.font!.lineHeight * amountOfLinesShown
         messageField.sizeThatFits(CGSize(width: 315, height: maxHeight))
     }
+    
+    // ================ TEXTVIEW BEGIN EDIT ========================
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            returnPressed += 1
+            if returnPressed < 17 {
+                messageField.frame = CGRect(x: 10, y: 10, width: messageField.frame.size.width, height: messageField.frame.size.height + 17)
+                newLine = 17 * returnPressed
+                UIView.animate(withDuration: 0.1, animations: { () -> Void in
+                    self.Dock.transform = CGAffineTransform(translationX: 0, y: CGFloat(-208 - self.newLine))
+                })
+            }
+        }
+        
+        return true
+    }
+    
+//    func textViewDidChange(_ textView: UITextView) {
+//        
+//        let pos: UITextPosition? = messageField.endOfDocument
+//        let currentRect: CGRect = messageField.caretRect(for: pos!)
+//        if currentRect.origin.y > previousRect.origin.y || (messageField.text == "\n") {
+//            returnPressed += 1
+//            if returnPressed < 3 {
+//                messageField.frame = CGRect(x: 5, y: 5, width: messageField.frame.size.width, height: messageField.frame.size.height + 17)
+//                newLine = 17 * returnPressed
+//                UIView.animate(withDuration: 0.1, animations: { () -> Void in
+//                    self.Dock.transform = CGAffineTransform(translationX: 0, y: CGFloat(-208 - self.newLine))
+//                })
+//            }
+//        }
+//        
+//        previousRect = currentRect
+//        
+//    }
+//    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        if (messageField.text == "") || (messageField.text == "Compose a message...") {
+            messageField.text = ""
+        }
+        messageField.textColor = UIColor.black
+        UIView.animate(withDuration: 0.209, animations: { () -> Void in
+            self.Dock.transform = CGAffineTransform(translationX: 0, y: CGFloat(-208 - self.newLine))
+        }, completion: { (_ finished: Bool) -> Void in
+        })
+        
+        return true
+        
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch: UITouch? = touches.first
+        if touch?.phase == .began {
+            messageField.resignFirstResponder()
+            view.endEditing(true)
+            let height: Int = returnPressed * 20
+            UIView.animate(withDuration: 0.209, animations: { () -> Void in
+                self.Dock.transform = CGAffineTransform(translationX: 0, y: CGFloat(-height))
+            })
+            
+            if (messageField.text == "") {
+                
+                messageField.textColor = UIColor.lightGray
+                messageField.text = "Compose a message..."
+                
+            }
+            
+        }
+        
+    }
+    
+    // ================ TEXTVIEW END EDIT ==========================
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
@@ -55,6 +146,17 @@ class ConvoViewController: UIViewController, UICollectionViewDelegate, UICollect
     
     @IBAction func onScreenTap(_ sender: Any) {
         view.endEditing(true)
+        let height: Int = returnPressed * 20
+        UIView.animate(withDuration: 0.209, animations: { () -> Void in
+            self.Dock.transform = CGAffineTransform(translationX: 0, y: CGFloat(-height))
+        })
+        
+        if (messageField.text == "") {
+            
+            messageField.textColor = UIColor.lightGray
+            messageField.text = "Compose a message..."
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
