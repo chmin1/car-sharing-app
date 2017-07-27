2fc64e94f739b5016c9dd28a86f459023350479e
Annabel Strauss <annabelstrauss@fb.com>
2017-07-27 10:26:20 -0700

    cleaned up code

--- CarSharingApp/YourTripsViewController.swift
+++ CarSharingApp/YourTripsViewController.swift
@@ -96,7 +96,7 @@ class YourTripsViewController: UIViewController, UITableViewDelegate, UITableVie
             cell.tripMembersLabel.text = memberString
             
             let memberProfPics = Helper.returnMemberProfPics(tripMembers: tripMembers)
-            displayProfilePics(withCell: cell, withMemberPics: memberProfPics)
+            Helper.displayProfilePics(withCell: cell, withMemberPics: memberProfPics)
         }
         
         cell.tripName.text = tripName
@@ -107,91 +107,6 @@ class YourTripsViewController: UIViewController, UITableViewDelegate, UITableVie
         return cell
     }
     
-    func displayProfilePics(withCell cell: TripCell, withMemberPics pics: [PFFile]){
-        let count = pics.count
-        cell.onePersonImageView.isHidden = true
-        cell.twoPeopleImageView1.isHidden = true
-        cell.twoPeopleImageView2.isHidden = true
-        cell.threePeopleImageView1.isHidden = true
-        cell.threePeopleImageView2.isHidden = true
-        cell.threePeopleImageView3.isHidden = true
-        cell.fourPeopleImageView1.isHidden = true
-        cell.fourPeopleImageView2.isHidden = true
-        cell.fourPeopleImageView3.isHidden = true
-        cell.fourPeopleImageView4.isHidden = true
-        
-        if(count == 1){
-            cell.onePersonImageView.isHidden = false
-            let profPic = pics[0]
-            profPic.getDataInBackground(block: { (data: Data?, error: Error?) in
-                cell.onePersonImageView.image = UIImage(data: data!)
-            })
-        } else if (count == 2) {
-            let profPic1 = pics[0]
-            let profPic2 = pics[1]
-            profPic1.getDataInBackground(block: { (data: Data?, error: Error?) in
-                cell.twoPeopleImageView1.image = UIImage(data: data!)
-            })
-            profPic2.getDataInBackground(block: { (data: Data?, error: Error?) in
-                cell.twoPeopleImageView2.image = UIImage(data: data!)
-            })
-            cell.twoPeopleImageView1.isHidden = false
-            cell.twoPeopleImageView2.isHidden = false
-        } else if (count == 3) {
-            let profPic1 = pics[0]
-            let profPic2 = pics[1]
-            let profPic3 = pics[2]
-            cell.threePeopleImageView1.isHidden = false
-            cell.threePeopleImageView2.isHidden = false
-            cell.threePeopleImageView3.isHidden = false
-            profPic1.getDataInBackground(block: { (data: Data?, error: Error?) in
-                cell.threePeopleImageView1.image = UIImage(data: data!)
-            })
-            profPic2.getDataInBackground(block: { (data: Data?, error: Error?) in
-                cell.threePeopleImageView2.image = UIImage(data: data!)
-            })
-            profPic3.getDataInBackground(block: { (data: Data?, error: Error?) in
-                cell.threePeopleImageView3.image = UIImage(data: data!)
-            })
-        } else if (count == 4) {
-            let profPic1 = pics[0]
-            let profPic2 = pics[1]
-            let profPic3 = pics[2]
-            let profPic4 = pics[3]
-            cell.fourPeopleImageView1.isHidden = false
-            cell.fourPeopleImageView2.isHidden = false
-            cell.fourPeopleImageView3.isHidden = false
-            cell.fourPeopleImageView4.isHidden = false
-            profPic1.getDataInBackground(block: { (data: Data?, error: Error?) in
-                cell.fourPeopleImageView1.image = UIImage(data: data!)
-            })
-            profPic2.getDataInBackground(block: { (data: Data?, error: Error?) in
-                cell.fourPeopleImageView2.image = UIImage(data: data!)
-            })
-            profPic3.getDataInBackground(block: { (data: Data?, error: Error?) in
-                cell.fourPeopleImageView3.image = UIImage(data: data!)
-            })
-            profPic4.getDataInBackground(block: { (data: Data?, error: Error?) in
-                cell.fourPeopleImageView4.image = UIImage(data: data!)
-            })
-        }
-        
-    }
-    
-    /*
-    
-    //======== TURNS ARRAY OF MEMBERS' PROF PICS ========
-    func returnMemberProfPics(tripMembers: [PFUser]) -> [PFFile] {
-        var memberPics: [PFFile] = []
-        for member in tripMembers {
-            print("HI")
-            if let profPic = member["profPic"] as? PFFile {
-                memberPics.append(profPic)
-            }
-        }
-        return memberPics
-    }
- */
     
     
     /*
@@ -205,18 +120,6 @@ class YourTripsViewController: UIViewController, UITableViewDelegate, UITableVie
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    /*
-    //======== TURNS ARRAY OF MEMBERS FROM PFUSER TO STRING ========
-    func returnMemberNames(tripMembers: [PFUser]) -> [String] {
-        var memberNames: [String] = []
-        for member in tripMembers {
-            if let memberName = member["fullname"] as? String {
-                memberNames.append(memberName)
-            }
-        }
-        return memberNames
-    }
-    */
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
