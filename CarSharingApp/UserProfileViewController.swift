cd15f97e408e93b546eec2eb0cfaeb9915f301cc
Elan Halpern <elanh@fb.com>
2017-08-03 18:48:44 -0700

    Added the user trips to the correct page, except the past trips tab is not working yet.

--- CarSharingApp/UserProfileViewController.swift
+++ CarSharingApp/UserProfileViewController.swift
@@ -9,16 +9,16 @@
 import UIKit
 import Parse
 
-class UserProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDelegate { //UITableViewDataSource {
+class UserProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDelegate, UITableViewDataSource {
     
     
     @IBOutlet weak var profilePicImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var mySegmentedControl: UISegmentedControl!
     @IBOutlet weak var tripsTableView: UITableView!
-    let list1: [String] = ["hi", "yo"]
-    let list2: [String] = ["annbel", "strauss"]
     var user: PFUser!
+    var currentTrips: [PFObject] = []
+    var pastTrips: [PFObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +27,8 @@ class UserProfileViewController: UIViewController, UIImagePickerControllerDelega
         self.navigationController?.navigationBar.tintColor = UIColor.white
         
         tripsTableView.delegate = self
+        tripsTableView.dataSource = self
+        refresh()
         
         //make prof pic circular
         profilePicImageView.layer.cornerRadius = profilePicImageView.frame.size.width / 2
@@ -41,6 +43,45 @@ class UserProfileViewController: UIViewController, UIImagePickerControllerDelega
         self.navigationController?.navigationBar.barTintColor = Helper.coral()
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         
+    }
+    
+    func refresh() {
+        //activityIndicator.startAnimating()
+        let query = PFQuery(className: "Trip")
+        query.includeKey("Name")
+        query.includeKey("Members")
+        query.whereKey("Members", equalTo: user!) //BUGGY I THINK
+        query.findObjectsInBackground { (trips: [PFObject]?, error: Error?) in
+            if let trips = trips {
+                self.currentTrips.removeAll()
+                self.pastTrips.removeAll()
+                for trip in trips {
+                    if let tripEditId = trip["EditID"] as? String { //get EditID so that the trip won't show if it's an edit
+                        if(tripEditId != "-1" && !Helper.isPastTrip(trip: trip)){ //only add trip to the feed if it's NOT an edit
+                            self.currentTrips.append(trip)
+                        } else if(tripEditId != "-1" && Helper.isPastTrip(trip: trip)) {
+                            self.pastTrips.append(trip)
+                            
+                        } else {
+                            print(error?.localizedDescription)
+                        }
+                    }
+                    
+                }
+                //displays the "No trips" label if there are no trips to display
+                /*
+                 if self.yourTrips.count == 0 {
+                 Helper.displayEmptyTableView(withTableView: self.yourTripsTableView, withText: "You are not currently in any trips!")
+                 self.emojiView.isHidden = false
+                 }
+                 */
+                
+                
+                self.tripsTableView.reloadData()
+                //self.refreshControl.endRefreshing()
+                //self.activityIndicator.stopAnimating()
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -63,10 +104,10 @@ class UserProfileViewController: UIViewController, UIImagePickerControllerDelega
         switch(mySegmentedControl.selectedSegmentIndex)
         {
         case 0:
-            returnVal = list1.count
+            returnVal = currentTrips.count
             break
         case 1:
-            returnVal = list2.count
+            returnVal = pastTrips.count
             break
         default:
             break
@@ -76,15 +117,75 @@ class UserProfileViewController: UIViewController, UIImagePickerControllerDelega
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let myCell = tripsTableView.dequeueReusableCell(withIdentifier: "profilePageCell", for: indexPath)
+        let myCell = tripsTableView.dequeueReusableCell(withIdentifier: "TripCell", for: indexPath) as! TripCell
         
         switch(mySegmentedControl.selectedSegmentIndex)
         {
         case 0:
-            myCell.textLabel?.text = list1[indexPath.row]
+            let trip = currentTrips[indexPath.row]
+            let tripName = trip["Name"] as! String
+            
+            let departureLocation = trip["DepartureLoc"] as! String
+            let arrivalLocation = trip["ArrivalLoc"] as! String
+            let earliestDepart = trip["EarliestTime"] as! String
+            let latestDepart = trip["LatestTime"] as! String
+            if let tripMembers = trip["Members"] as? [PFUser] {
+                print(tripName)
+                let memberNames = Helper.returnMemberNames(tripMembers: tripMembers)
+                print(memberNames)
+                var memberString = ""
+                
+                for memberName in memberNames {
+                    memberString += memberName.capitalized
+                    if memberName != memberNames.last {
+                        memberString += ", "
+                    }
+                }
+                myCell.tripMembersLabel.text = memberString
+                
+                let memberProfPics = Helper.returnMemberProfPics(tripMembers: tripMembers)
+                Helper.displayProfilePics(withCell: myCell, withMemberPics: memberProfPics)
+            }
+            
+            
+            myCell.tripName.text = tripName.capitalized
+            myCell.departLabel.text = departureLocation
+            myCell.destinationLabel.text = arrivalLocation
+            myCell.earlyTimeLabel.text = earliestDepart
+            myCell.lateDepartLabel.text = latestDepart
             break
         case 1:
-            myCell.textLabel?.text = list2[indexPath.row]
+            let trip = pastTrips[indexPath.row]
+            let tripName = trip["Name"] as! String
+            
+            let departureLocation = trip["DepartureLoc"] as! String
+            let arrivalLocation = trip["ArrivalLoc"] as! String
+            let earliestDepart = trip["EarliestTime"] as! String
+            let latestDepart = trip["LatestTime"] as! String
+            if let tripMembers = trip["Members"] as? [PFUser] {
+                print(tripName)
+                let memberNames = Helper.returnMemberNames(tripMembers: tripMembers)
+                print(memberNames)
+                var memberString = ""
+                
+                for memberName in memberNames {
+                    memberString += memberName.capitalized
+                    if memberName != memberNames.last {
+                        memberString += ", "
+                    }
+                }
+                myCell.tripMembersLabel.text = memberString
+                
+                let memberProfPics = Helper.returnMemberProfPics(tripMembers: tripMembers)
+                Helper.displayProfilePics(withCell: myCell, withMemberPics: memberProfPics)
+            }
+            
+            
+            myCell.tripName.text = tripName.capitalized
+            myCell.departLabel.text = departureLocation
+            myCell.destinationLabel.text = arrivalLocation
+            myCell.earlyTimeLabel.text = earliestDepart
+            myCell.lateDepartLabel.text = latestDepart
             break
         default:
             break
